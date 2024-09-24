fe917aa4a16caa867455b6313b1a4033a5f0b203
Devilwining <159485044+Devilwining@users.noreply.github.com>
2024-09-24 20:00:11 -0300

    web site em C#.net 24/09/2024
    web site em c# .net 6 Utiliza razor e bootstrap  Uma tela de cadastro Nome  CPF E-mail Telefone Instagram Endereço completo  Faz banco sql server

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CntroleDeContatos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
